473c4f02352519f1022046893b1eb90f6511b4fb
Guillermo Gómez <guillermo.gf@openmailbox.org>
2015-01-11 17:36:23 +0100

    When terminated stop all processes started and stop transmitting

--- PirateRadio.py
+++ PirateRadio.py
@@ -13,6 +13,8 @@ import re
 import random
 import threading
 import time
+import signal
+import RPi.GPIO as gpio
 
 
 fm_process = None
@@ -32,6 +34,7 @@ microphone_pipe_r, microphone_pipe_w = os.pipe()
 
 def main():
     daemonize()
+    print("To stop run 'kill -15 {0}'".format(os.getpid()))
     setup()
     files = build_file_list()
     if repeat_all:
@@ -65,29 +68,34 @@ def play_songs(file_list):
     with open(os.devnull, "w") as dev_null:
         for filename in file_list:
             print("Playing ", filename)
+            global ffmpeg
             if re.search(".pls$", filename) is not None:
                 streamurl = parse_pls(filename, 1)
                 if streamurl is not None:
                     print("streaming radio from " + streamurl)
-                    subprocess.call(["ffmpeg", "-i", streamurl, "-f", "s16le",
-                                     "-acodec", "pcm_s16le", "-ac",
-                                     "2" if play_stereo else "1", "-ar",
-                                     "44100", "-"], stdout=music_pipe_w,
-                                    stderr=dev_null)
+                    ffmpeg = subprocess.Popen(["ffmpeg", "-i", streamurl, "-f",
+                                               "s16le", "-acodec", "pcm_s16le",
+                                               "-ar", "44100", "-ac",
+                                               "2" if play_stereo else "1",
+                                               "-"], stdout=music_pipe_w,
+                                              stderr=dev_null)
             elif re.search(".m3u$", filename) is not None:
                 streamurl = parse_m3u(filename, 1)
                 if streamurl is not None:
                     print("streaming radio from " + streamurl)
-                    subprocess.call(["ffmpeg", "-i", streamurl, "-f", "s16le",
-                                     "-acodec", "pcm_s16le", "-ac",
-                                     "2" if play_stereo else "1", "-ar",
-                                     "44100", "-"], stdout=music_pipe_w,
-                                    stderr=dev_null)
+                    ffmpeg = subprocess.Popen(["ffmpeg", "-i", streamurl, "-f",
+                                               "s16le", "-acodec", "pcm_s16le",
+                                               "-ar", "44100", "-ac",
+                                               "2" if play_stereo else "1",
+                                               "-"], stdout=music_pipe_w,
+                                              stderr=dev_null)
             else:
-                subprocess.call(["ffmpeg", "-i", filename, "-f", "s16le",
-                                 "-acodec", "pcm_s16le", "-ac",
-                                 "2" if play_stereo else "1", "-ar", "44100",
-                                 "-"], stdout=music_pipe_w, stderr=dev_null)
+                ffmpeg = subprocess.Popen(["ffmpeg", "-i", filename, "-f",
+                                           "s16le", "-acodec", "pcm_s16le",
+                                           "-ac", "2" if play_stereo else "1",
+                                           "-ar", "44100", "-"],
+                                          stdout=music_pipe_w, stderr=dev_null)
+            ffmpeg.wait()
 
 
 def read_config():
@@ -212,4 +220,17 @@ def open_microphone():
         run_pifm()
 
 
+def terminate(signum, frame):
+    # This will stop the transmission not just silencing it
+    gpio.setmode(gpio.BOARD)
+    gpio.setwarnings(False)
+    gpio.setup(7, gpio.OUT)
+    gpio.cleanup()
+
+    fm_process.terminate()  # Stop pifm
+    ffmpeg.kill()  # Stop ffmpeg. Terminate() didn't always work
+    sys.exit()
+
+
+signal.signal(signal.SIGTERM, terminate)
 main()
